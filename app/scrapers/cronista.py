6add6f3681f3d8964e87378a77884aa73318d493
alan <alan.g.melofreyre@gmail.com>
2023-01-11 02:04:52 -0300

    Add docstrings and exceptions

--- app/scrapers/cronista.py
+++ app/scrapers/cronista.py
@@ -1,25 +1,92 @@
 from datetime import datetime
-from .base import base_scraping
+import requests
+from lxml import html
 
-def dolarhoy_scraping():
-    url = "https://www.cronista.com/MercadosOnline/dolar.html"
-    xpath = 'response.xpath("//tr")'
+import logging
+from pathlib import Path
 
-    dolars_list = base_scraping(url, xpath)
+log_file = str(Path(__file__).parent.joinpath("logs_web_scraping.log"))
 
-    date_now = datetime.now()
-    date = date_now.strftime("%d/%m/%Y %H:%M:%S")
+logging.basicConfig(
+    level=logging.DEBUG,
+    filename=log_file,
+    filemode="a",
+    format='%(asctime)s - %(levelname)s - %(filename)s -%(message)s',
+    datefmt="%Y-%m-%dT%H:%M:%S%z"
+)
 
-    list_jsn=[]
+def cronista_scraping() -> list[dict] | None:
+    """ Get the dollar values on the page
+
+    :return: list with a json that contains the values of purchase,
+            sale and date of each dollar
+    :rtype: list[dict]
+    """
+
+    url = "https://www.cr" + "onista.com/MercadosOnline/dolar.html"
+
+    # Request url
+    try:
+        response = requests.get(url)
+    except Exception as e:
+        logging.error("Error with Cro" + "nista url: " + str(e))
+        return None
+    
+    try:
+        parser = html.fromstring(response.content)
+    except Exception as e:
+        logging.error("Error procecing Cro" + "nista response" + str(e))
+        return None
+
+    # Get data from html
+    dollar_name_list = parser.xpath(
+        '//tr//td[@class="name"]/a/text()'
+        )
+    logging.info(f"Extracted dollar names: {len(dollar_name_list)}")
+    dollar_buy_list = parser.xpath(
+        '//tr//td[@class="buy"]/a//div[@class="buy-value"]/text()'
+        )
+    logging.info(f"Purchase prices of dollars extracted: {len(dollar_buy_list)}")
+    dollar_sell_list = parser.xpath(
+        '//tr//td[@class="sell"]/a//div[@class="sell-value"]/text()'
+        )
+    logging.info(f"Sell prices of dollars extracted: {len(dollar_sell_list)}")
+    dollar_date_list = parser.xpath(
+        '//tr//td[@class="date"]/a/text()'
+        )
+    logging.info(f"Extracted update dates: {len(dollar_date_list)}")
+    
+    # Structure the data
+    list_jsn = []
     jsn = {}
 
-    for p in range(0,len(dolars_list),3):
-        jsn[dolars_list[p]] = {
-            "compra" : dolars_list[p+1],
-            "venta" : dolars_list[p+2],
-            "date" : date
-        }
+    try:
+        for position in range(len(dollar_name_list)):
+            
+            if dollar_name_list[position] == "DÓLAR TURISTA":
 
+                jsn[dollar_name_list[position]] = {
+                    "venta": dollar_sell_list[position],
+                    "date": dollar_date_list[position],
+                }
+
+                # A number is added in the position to balance
+                # the length of the lists, due to the fact that
+                # "tourist dollar" is the only dollar that
+                #  does not have a purchase price 
+                dollar_buy_list.insert(0,position)
+
+            else:
+                jsn[dollar_name_list[position]] = {
+                        "compra": dollar_buy_list[position],
+                        "venta": dollar_sell_list[position],
+                        "date": dollar_date_list[position],
+                    }
+        logging.info("Data was successfully structured")
+    except Exception as e:
+        logging.error("Error structuring data: " + str(e))
+        return None
+    
     list_jsn.append(jsn)
 
     return list_jsn
--- app/scrapers/dolarhoy.py
+++ app/scrapers/dolarhoy.py
@@ -1,26 +1,73 @@
+import requests
 from datetime import datetime
-from .base import base_scraping
+import logging
 
-def dolarhoy_scraping() -> list[dict]:
+from lxml import html
+from pathlib import Path
 
-    url = "https://dolarhoy.com/cotizacion-dolar-blue"
-    xpath = '//div[@class="tile is-child"]/a[contains(@href,"dolar")]/div/text()'
+log_file = str(Path(__file__).parent.joinpath("logs_web_scraping.log"))
 
-    dolars_list = base_scraping(url, xpath)
+logging.basicConfig(
+    level=logging.DEBUG,
+    filename=log_file,
+    filemode="a",
+    format='%(asctime)s - %(levelname)s - %(filename)s -%(message)s',
+    datefmt="%Y-%m-%dT%H:%M:%S%z"
+)
+
+
+def dolarhoy_scraping() -> list[dict] | None:
+    """ Get the dollar values on the page
+
+    :return: list with a json that contains the values of purchase,
+            sale and date of each dollar
+    :rtype: list[dict]
+    """
+
+    url = "https://dola" + "rhoy.com/cotizacion-do" + "lar-blue"
+    xpath = r"""//div[@class="tile is-child"]
+                /a[contains(@href,"dolar")]/div/text()"""
+
+    # Request url
+    try:
+        response = requests.get(url)
+    except Exception as e:
+        logging.error("Error with Dol" + "arhoy url: " + str(e))
+        return None
+
+    try:
+        response = response.content.decode("utf-8")
+        parser = html.fromstring(response)
+    except Exception as e:
+        logging.error("Error procecing dol" + "arhoy response" + str(e))
+        return None
 
     date_now = datetime.now()
     date = date_now.strftime("%d/%m/%Y %H:%M:%S")
 
-    list_jsn=[]
+    # Collect data
+    dollar_values_list = parser.xpath(xpath)
+    logging.info(f"Extracted values: {len(dollar_values_list)}")
+
+    # Structure the data
+    list_jsn = []
     jsn = {}
-    
-    for p in range(0, len(dolars_list), 3):
-        jsn[dolars_list[p]] = {
-            "compra" : dolars_list[p+1],
-            "venta" : dolars_list[p+2],
-            "date" : date
-        }
 
+    try:
+
+        for position_number in range(0, len(dollar_values_list), 3):
+
+            # Save the values in the json
+            jsn[dollar_values_list[position_number]] = {
+                "compra": dollar_values_list[position_number+1],
+                "venta": dollar_values_list[position_number+2],
+                "date": date
+            }
+        logging.info("Data was successfully structured")
+    except Exception as e:
+        logging.error("Error getting data from html: " + str(e))
+        return None
+
+    # Put the json in a list to be able to save it in mongodb
     list_jsn.append(jsn)
-
     return list_jsn
