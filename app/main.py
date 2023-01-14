c2278fd35d1ac01e265615cf0fb48aeeeebf8231
alan <alan.g.melofreyre@gmail.com>
2023-01-14 01:52:11 -0300

    removes some endpoints and improves others

--- app/main.py
+++ app/main.py
@@ -1,197 +1,77 @@
-
-# Python
-from typing import Optional
-
 from fastapi import FastAPI
-from fastapi import Path
 from fastapi_utils.tasks import repeat_every
 
-from enum import Enum
-from .config.db import *
+from .config import db
 
+from .routers.cronista import cronista_router
+from .routers.dolarhoy import dolarhoy_router
+from .routers.root import root_router
 
-from .scrapers.infobae import infobae_scraping
 from .scrapers.dolarhoy import dolarhoy_scraping
+from .scrapers.cronista import cronista_scraping
 
 app = FastAPI()
-
-dicconario = {
-    "dolar_libre" : "Dólar Libre",
-    "dolar_targeta" : "Dólar tarjeta",
-    "dolar_mep" : "Dólar MEP",
-    "ccl" : "Contado con liqui",
-    "banco_nacion" : "Dólar Banco Nación",
-    "riesgo_pais" : "Riesgo País",
-    "dolar_mayorista" : "Dólar Mayorista",
-    "dolar_bolsa" : "Dólar Bolsa",
-    "banco_nacion" : "Banco Nación",
-}
+app.include_router(dolarhoy_router)
+app.include_router(cronista_router)
+app.include_router(root_router)
 
 
-class Dolares_dolarhoy(str, Enum):
-    dolar_libre = "dolar_libre"
-    dolar_mayorista = "dolar_mayorista"
-    dolar_bolsa = "dolar_bolsa"
-    ccl = "ccl"
-    banco_nacion = "banco_nacion"
+def compare_last_date(collection, data: dict) -> dict:
+    """ gets the last values of the dollars loaded on the page
 
-class Dolares_infobae(str, Enum):
-    dolar_libre = "dolar_libre"
-    dolar_targeta = "dolar_targeta"
-    dolar_mep = "dolar_mep"
-    ccl = "ccl"
-    banco_nacion = "banco_nacion"
-    riesgo_pais = "riesgo_pais"
+    :param collection: Mongo collection
+    :type collection: pymongo.collection
+    :param page: page name
+    :type page: str
+    :return: returns a dict with page
+        as key and last values from collection as value
+    :rtype: dict
+    """
+
+    try:
+        last_documents_in_db = collection.find({}).sort(
+            [('$natural', -1)]).limit(1)
+
+        if len(list(last_documents_in_db)) <= 0:
+            return False
+
+        for document in last_documents_in_db:
+
+            if (
+                data["dólar libre"] and
+                data["dólar libre"]["date"]
+            ) is False and (
+                    document["dólar libre"] and
+                    document["dólar libre"]["date"]
+            ) is False:
+
+                return False
+
+            if data["dólar libre"]["date"] != document["dólar libre"]["date"]:
+                return False
+
+            return True
+
+    except Exception as e:
+        print("Error: " + str(e))
+        return False
 
 
 @app.on_event("startup")
-@repeat_every(seconds= 60*20 ,wait_first=False)
-def save_infobae_dollars_values():
+@repeat_every(seconds=60*1, wait_first=False)
+def save_dolarhoy_dollars_values() -> None:
 
-    # Scraping Infobae
-    #db = client["dolares"]
-    list_of_dollars = infobae_scraping()
-    #collec = db["infobae"]
-    collection_infobae.insert_many(list_of_dollars)
+    dolarhoy_dollars_data = dolarhoy_scraping()
+    cronista_dollars_data = cronista_scraping()
 
-    return 0
+    if compare_last_date(
+                        db.db["dolarhoy"],
+                        dolarhoy_dollars_data
+                        ) is not True:
+        db.db["dolarhoy"].insert_one(dolarhoy_dollars_data)
 
-@app.on_event("startup")
-@repeat_every(seconds= 60*20 ,wait_first=False)
-def save_dolarhoy_dollars_values():
-
-    # Dolarhoy
-    #db = client["dolares"]
-    list_of_dollars = dolarhoy_scraping()
-    #collecc = db["dolarhoy"]
-    collection_dolarhoy.insert_many(list_of_dollars)
-
-    return 0
-
-
-def get_last_dolar_libre_value(collection, pagina: str, lis: list):
-    """
-    takes the last document in the collection, adds the "Page" element 
-    and returns the list adding this document in the last position.
-    """
-
-    for documento in  collection.find({}).limit(1).sort([('$natural',-1)]):
-
-        d = documento[dicconario["dolar_libre"]]
-        d["Pagina"] = pagina
-        lis.append(d)
-
-    return lis
-
-@app.get("/")
-def lastest_values():
-    """
-    Return the latest free dollar values that pages have
-    """
-    info = "infobae"
-    dolarhoy = "dolarhoy"
-    response_ = []
-    
-    response_ = get_last_dolar_libre_value(
-                                            collection_dolarhoy, 
-                                            dolarhoy, 
-                                            response_
-                                            )
-
-    response_ = get_last_dolar_libre_value(
-                                            collection_infobae,
-                                            info,
-                                            response_
-                                            )
-
-    return response_
-
-
-@app.get("/infobae/{dolar}")
-def infobae_dolar(
-    dolar : Dolares_infobae = Path(
-        ...,
-        title = "Name of dollars to get",
-    ),
-):
-    """
-    Shows all values of the specified dollar collected from infobae.com
-    """   
-    dolar_=dicconario[dolar]
-    response_ = []
-    for documento in  collection_infobae.find({}).sort([('$natural',-1)]):
-        response_.append(documento[dolar_])
-
-    return response_
-
-@app.get("/infobae/{dolar}/{amount}")
-def infobae_dolar_amount(
-    dolar : Dolares_infobae = Path(
-        ...,
-        title = "Name of dollars to get",
-    ),
-    amount : Optional[int] = Path(
-        ...,
-        gt=0,
-        le=101,
-        title="Amounts of values of dollars to get",
-        description="Integers between zero and hundred"
-    )  
-):
-    """
-    Return, according to the specified amount,
-    the values of the required dollar values collected from infobae.com
-    """
-
-    dolar_= dicconario[dolar]
-    response_ = []
-
-    for documento in  collection_infobae.find({}).limit(amount).sort([('$natural',-1)]):
-        response_.append(documento[dolar_])
-
-    return response_
-
-@app.get("/dolarhoy/{dolar}")
-def dolarhoy_dolar(
-    dolar : Dolares_dolarhoy = Path(
-        ...,
-        title = "Name of dollars to get",
-    ),
-):
-    """
-    Shows all values of the specified dollar collected from dolarhoy.com
-    """
-    dolar_= dicconario[dolar]
-    response_ = []
-    for documento in  collection_dolarhoy.find({}).sort([('$natural',-1)]):
-        response_.append(documento[dolar_])
-        
-    return response_
-
-@app.get("/dolarhoy/{dolar}/{amount}")
-def dolarhoy_dolar_amount(
-    dolar : Dolares_dolarhoy = Path(
-        ...,
-        title = "Name of dollars to get",
-        
-    ),
-    amount : Optional[int] = Path(
-        ...,
-        gt=0,
-        le=101,
-        title="Amounts of values of dollars to get",
-        description="Integers between zero and hundred"
-    )  
-):  
-    """
-    Return, according to the specified amount,
-    the values of the required dollar values collected from dolarhoy.com
-    """
-    dolar_= dicconario[dolar]
-    response_ = []    
-    for documento in  collection_dolarhoy.find({}).limit(amount).sort([('$natural',-1)]):
-        response_.append(documento[dolar_])
-
-    return response_
-
-
+    if compare_last_date(
+                        db.db["cronista"],
+                        cronista_dollars_data
+                    ) is not True:
+        db.db["cronista"].insert_one(cronista_dollars_data)
