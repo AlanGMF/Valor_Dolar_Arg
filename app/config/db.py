89fb53af8ad771cd1a5597bbd38af8d8d48b4534
alan <alan.g.melofreyre@gmail.com>
2023-01-14 11:59:58 -0300

    add decouple config

--- app/config/db.py
+++ app/config/db.py
@@ -1,7 +1,13 @@
 from pymongo import MongoClient
+from decouple import config
 
-client = MongoClient(
-    "mongodb://admin:password@mon:27017/"
-    )
+USER = config('USER')
+PASSWORD = config('PASSWORD')
+CONTAINER_NAME = config('CONTAINER_NAME')
+CONTAINER_PORT = config('CONTAINER_PORT')
+
+URL_STR = f"mongodb://{USER}:{PASSWORD}@{CONTAINER_NAME}:{CONTAINER_PORT}/"
+
+client = MongoClient(URL_STR)
 
 db = client.dolares
